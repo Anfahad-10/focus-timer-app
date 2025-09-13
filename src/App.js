119a787eb5e8052e2da505e16220d101a8898256
Fahad <fahadanwar5925@gmail.com>
2025-09-13 21:12:45 +0530

    feat: Created Timer component and render in App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
+
+import Timer from './components/Timer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-  <h1>My Focus Timer App</h1>
-  <p>The journey begins!</p>
-</header>
+        <h1>My Focus Timer App</h1>
+        {/* 2. Use the component like an HTML tag */}
+        <Timer />
+      </header>
     </div>
   );
 }
---
+++ src/components/Timer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Timer() {
+  return (
+    <div className="timer-container">
+      <h2>Timer Component</h2>
+      <h1>00:00</h1>
+      <button>Start</button>
+      <button>Stop</button>
+    </div>
+  );
+}
+
+export default Timer;
